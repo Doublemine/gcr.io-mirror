a15913f9421dd428e01627752be885ea191d227d
Doublemine <doublemine.w@gmail.com>
2018-09-06 11:57:50 +0800

    fix

--- util/common.py
+++ util/common.py
@@ -179,7 +179,7 @@ def update_trigger(trigger=False, force=False):
 
 
 def read_trigger():
-    synced = load_jsond('trigger.json', False, False)
+    synced = load_jsond('trigger.json', True, False)
     if synced is None:
         return True
     else:
